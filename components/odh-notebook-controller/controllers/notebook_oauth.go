cf2ea9cf72c5cef4ff118a5040c9f171adb7d022
Samuel Veloso <svelosol@redhat.com>
2022-03-22 16:27:32 +0100

    Implement odh-notebook-controller
    The new controller will watch the **Kubeflow Notebook** custom resource events to extend the Kubeflow notebook controller behavior with the following capabilities:
    
    - Openshift ingress controller integration.
    - Openshift OAuth sidecar injection.
    
    Fix https://github.com/opendatahub-io/kubeflow/issues/8
    
    Signed-off-by: Samuel Veloso <svelosol@redhat.com>

---
+++ components/odh-notebook-controller/controllers/notebook_oauth.go
@@ -0,0 +1,263 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"reflect"
+
+	"k8s.io/apimachinery/pkg/util/intstr"
+
+	nbv1 "github.com/kubeflow/kubeflow/components/notebook-controller/api/v1"
+	routev1 "github.com/openshift/api/route/v1"
+	corev1 "k8s.io/api/core/v1"
+	apierrs "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+const (
+	OAuthServicePort     = 443
+	OAuthServicePortName = "oauth-proxy"
+	OAuthProxyImage      = "registry.redhat.io/openshift4/ose-oauth-proxy:latest"
+)
+
+type OAuthConfig struct {
+	ProxyImage string
+}
+
+// NewNotebookServiceAccount defines the desired service account object
+func NewNotebookServiceAccount(notebook *nbv1.Notebook) *corev1.ServiceAccount {
+	return &corev1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      notebook.Name,
+			Namespace: notebook.Namespace,
+			Labels: map[string]string{
+				"notebook-name": notebook.Name,
+			},
+			Annotations: map[string]string{
+				"serviceaccounts.openshift.io/oauth-redirectreference.first": "" +
+					`{"kind":"OAuthRedirectReference","apiVersion":"v1",` +
+					`"reference":{"kind":"Route","name":"` + notebook.Name + `"}}`,
+			},
+		},
+	}
+}
+
+// CompareNotebookServiceAccounts checks if two service accounts are equal, if
+// not return false
+func CompareNotebookServiceAccounts(sa1 corev1.ServiceAccount, sa2 corev1.ServiceAccount) bool {
+	// Two service accounts will be equal if the labels and annotations are
+	// identical
+	return reflect.DeepEqual(sa1.ObjectMeta.Labels, sa2.ObjectMeta.Labels) &&
+		reflect.DeepEqual(sa1.ObjectMeta.Annotations, sa2.ObjectMeta.Annotations)
+}
+
+// ReconcileOAuthServiceAccount will manage the service account reconciliation
+// required by the notebook OAuth proxy
+func (r *OpenshiftNotebookReconciler) ReconcileOAuthServiceAccount(notebook *nbv1.Notebook, ctx context.Context) error {
+	// Initialize logger format
+	log := r.Log.WithValues("notebook", notebook.Name, "namespace", notebook.Namespace)
+
+	// Generate the desired service account
+	desiredServiceAccount := NewNotebookServiceAccount(notebook)
+
+	// Create the service account if it does not already exist
+	foundServiceAccount := &corev1.ServiceAccount{}
+	err := r.Get(ctx, types.NamespacedName{
+		Name:      desiredServiceAccount.Name,
+		Namespace: notebook.Namespace,
+	}, foundServiceAccount)
+	if err != nil {
+		if apierrs.IsNotFound(err) {
+			log.Info("Creating Service Account")
+			// Add .metatada.ownerReferences to the service account to be deleted by
+			// the Kubernetes garbage collector if the notebook is deleted
+			err = ctrl.SetControllerReference(notebook, desiredServiceAccount, r.Scheme)
+			if err != nil {
+				log.Error(err, "Unable to add OwnerReference to the Service Account")
+				return err
+			}
+			// Create the service account in the Openshift cluster
+			err = r.Create(ctx, desiredServiceAccount)
+			if err != nil && !apierrs.IsAlreadyExists(err) {
+				log.Error(err, "Unable to create the Service Account")
+				return err
+			}
+		} else {
+			log.Error(err, "Unable to fetch the Service Account")
+			return err
+		}
+	}
+
+	return nil
+}
+
+// NewNotebookOAuthService defines the desired OAuth service object
+func NewNotebookOAuthService(notebook *nbv1.Notebook) *corev1.Service {
+	return &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      notebook.Name + "-tls",
+			Namespace: notebook.Namespace,
+			Labels: map[string]string{
+				"notebook-name": notebook.Name,
+			},
+			Annotations: map[string]string{
+				"service.beta.openshift.io/serving-cert-secret-name": notebook.Name + "-tls",
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{{
+				Name:       OAuthServicePortName,
+				Port:       OAuthServicePort,
+				TargetPort: intstr.FromString(OAuthServicePortName),
+				Protocol:   corev1.ProtocolTCP,
+			}},
+			Selector: map[string]string{
+				"statefulset": notebook.Name,
+			},
+		},
+	}
+}
+
+// CompareNotebookServices checks if two services are equal, if not return false
+func CompareNotebookServices(s1 corev1.Service, s2 corev1.Service) bool {
+	// Two services will be equal if the labels and annotations are identical
+	return reflect.DeepEqual(s1.ObjectMeta.Labels, s2.ObjectMeta.Labels) &&
+		reflect.DeepEqual(s1.ObjectMeta.Annotations, s2.ObjectMeta.Annotations)
+}
+
+// ReconcileOAuthService will manage the OAuth service reconciliation required
+// by the notebook OAuth proxy
+func (r *OpenshiftNotebookReconciler) ReconcileOAuthService(notebook *nbv1.Notebook, ctx context.Context) error {
+	// Initialize logger format
+	log := r.Log.WithValues("notebook", notebook.Name, "namespace", notebook.Namespace)
+
+	// Generate the desired OAuth service
+	desiredService := NewNotebookOAuthService(notebook)
+
+	// Create the OAuth service if it does not already exist
+	foundService := &corev1.Service{}
+	err := r.Get(ctx, types.NamespacedName{
+		Name:      desiredService.GetName(),
+		Namespace: notebook.GetNamespace(),
+	}, foundService)
+	if err != nil {
+		if apierrs.IsNotFound(err) {
+			log.Info("Creating OAuth Service")
+			// Add .metatada.ownerReferences to the OAuth service to be deleted by
+			// the Kubernetes garbage collector if the notebook is deleted
+			err = ctrl.SetControllerReference(notebook, desiredService, r.Scheme)
+			if err != nil {
+				log.Error(err, "Unable to add OwnerReference to the OAuth Service")
+				return err
+			}
+			// Create the OAuth service in the Openshift cluster
+			err = r.Create(ctx, desiredService)
+			if err != nil && !apierrs.IsAlreadyExists(err) {
+				log.Error(err, "Unable to create the OAuth Service")
+				return err
+			}
+		} else {
+			log.Error(err, "Unable to fetch the OAuth Service")
+			return err
+		}
+	}
+
+	return nil
+}
+
+// NewNotebookOAuthSecret defines the desired OAuth secret object
+func NewNotebookOAuthSecret(notebook *nbv1.Notebook) *corev1.Secret {
+	// Generate the cookie secret for the OAuth proxy
+	cookieSeed := make([]byte, 16)
+	rand.Read(cookieSeed)
+	cookieSecret := base64.StdEncoding.EncodeToString(
+		[]byte(base64.StdEncoding.EncodeToString(cookieSeed)))
+
+	// Create a Kubernetes secret to store the cookie secret
+	return &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      notebook.Name + "-oauth-config",
+			Namespace: notebook.Namespace,
+			Labels: map[string]string{
+				"notebook-name": notebook.Name,
+			},
+		},
+		StringData: map[string]string{
+			"cookie_secret": cookieSecret,
+		},
+	}
+}
+
+// ReconcileOAuthSecret will manage the OAuth secret reconciliation required by
+// the notebook OAuth proxy
+func (r *OpenshiftNotebookReconciler) ReconcileOAuthSecret(notebook *nbv1.Notebook, ctx context.Context) error {
+	// Initialize logger format
+	log := r.Log.WithValues("notebook", notebook.Name, "namespace", notebook.Namespace)
+
+	// Generate the desired OAuth secret
+	desiredSecret := NewNotebookOAuthSecret(notebook)
+
+	// Create the OAuth secret if it does not already exist
+	foundSecret := &corev1.Secret{}
+	err := r.Get(ctx, types.NamespacedName{
+		Name:      desiredSecret.Name,
+		Namespace: notebook.Namespace,
+	}, foundSecret)
+	if err != nil {
+		if apierrs.IsNotFound(err) {
+			log.Info("Creating OAuth Secret")
+			// Add .metatada.ownerReferences to the OAuth secret to be deleted by
+			// the Kubernetes garbage collector if the notebook is deleted
+			err = ctrl.SetControllerReference(notebook, desiredSecret, r.Scheme)
+			if err != nil {
+				log.Error(err, "Unable to add OwnerReference to the OAuth Secret")
+				return err
+			}
+			// Create the OAuth secret in the Openshift cluster
+			err = r.Create(ctx, desiredSecret)
+			if err != nil && !apierrs.IsAlreadyExists(err) {
+				log.Error(err, "Unable to create the OAuth Secret")
+				return err
+			}
+		} else {
+			log.Error(err, "Unable to fetch the OAuth Secret")
+			return err
+		}
+	}
+
+	return nil
+}
+
+// NewNotebookOAuthRoute defines the desired OAuth route object
+func NewNotebookOAuthRoute(notebook *nbv1.Notebook) *routev1.Route {
+	route := NewNotebookRoute(notebook)
+	route.Spec.To.Name = notebook.Name + "-tls"
+	route.Spec.Port.TargetPort = intstr.FromString(OAuthServicePortName)
+	route.Spec.TLS.Termination = routev1.TLSTerminationReencrypt
+	return route
+}
+
+// Reconcile will manage the creation, update and deletion of the OAuth route
+// when the notebook is reconciled
+func (r *OpenshiftNotebookReconciler) ReconcileOAuthRoute(
+	notebook *nbv1.Notebook, ctx context.Context) error {
+	return r.reconcileRoute(notebook, ctx, NewNotebookOAuthRoute)
+}
---
+++ components/odh-notebook-controller/controllers/notebook_webhook.go
@@ -0,0 +1,225 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	nbv1 "github.com/kubeflow/kubeflow/components/notebook-controller/api/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/utils/pointer"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+//+kubebuilder:webhook:path=/mutate-notebook-v1,mutating=true,failurePolicy=fail,sideEffects=None,groups=kubeflow.org,resources=notebooks,verbs=create;update,versions=v1,name=notebooks.opendatahub.io,admissionReviewVersions=v1
+
+// NotebookWebhook holds the webhook configuration.
+type NotebookWebhook struct {
+	Client      client.Client
+	decoder     *admission.Decoder
+	OAuthConfig OAuthConfig
+}
+
+// InjectOAuthProxy injects the OAuth proxy sidecar container in the Notebook
+// spec
+func InjectOAuthProxy(notebook *nbv1.Notebook, oauth OAuthConfig) error {
+	// https://pkg.go.dev/k8s.io/api/core/v1#Container
+	proxyContainer := corev1.Container{
+		Name:            "oauth-proxy",
+		Image:           oauth.ProxyImage,
+		ImagePullPolicy: corev1.PullAlways,
+		Env: []corev1.EnvVar{{
+			Name: "NAMESPACE",
+			ValueFrom: &corev1.EnvVarSource{
+				FieldRef: &corev1.ObjectFieldSelector{
+					FieldPath: "metadata.namespace",
+				},
+			},
+		}},
+		Args: []string{
+			"--provider=openshift",
+			"--https-address=:8443",
+			"--http-address=",
+			"--openshift-service-account=" + notebook.Name,
+			"--cookie-secret-file=/etc/oauth/config/cookie_secret",
+			"--cookie-expire=24h0m0s",
+			"--tls-cert=/etc/tls/private/tls.crt",
+			"--tls-key=/etc/tls/private/tls.key",
+			"--upstream=http://localhost:8888",
+			"--upstream-ca=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt",
+			"--skip-auth-regex=^/api$",
+			"--email-domain=*",
+			"--skip-provider-button",
+			`--openshift-sar={"verb":"get","resource":"notebooks","resourceAPIGroup":"kubeflow.org",` +
+				`"name":"` + notebook.Name + `","namespace":"$(NAMESPACE)"}`,
+		},
+		Ports: []corev1.ContainerPort{{
+			Name:          OAuthServicePortName,
+			ContainerPort: 8443,
+			Protocol:      corev1.ProtocolTCP,
+		}},
+		LivenessProbe: &corev1.Probe{
+			ProbeHandler: corev1.ProbeHandler{
+				HTTPGet: &corev1.HTTPGetAction{
+					Path:   "/oauth/healthz",
+					Port:   intstr.FromString(OAuthServicePortName),
+					Scheme: corev1.URISchemeHTTPS,
+				},
+			},
+			InitialDelaySeconds: 30,
+			TimeoutSeconds:      1,
+			PeriodSeconds:       5,
+			SuccessThreshold:    1,
+			FailureThreshold:    3,
+		},
+		ReadinessProbe: &corev1.Probe{
+			ProbeHandler: corev1.ProbeHandler{
+				HTTPGet: &corev1.HTTPGetAction{
+					Path:   "/oauth/healthz",
+					Port:   intstr.FromString(OAuthServicePortName),
+					Scheme: corev1.URISchemeHTTPS,
+				},
+			},
+			InitialDelaySeconds: 5,
+			TimeoutSeconds:      1,
+			PeriodSeconds:       5,
+			SuccessThreshold:    1,
+			FailureThreshold:    3,
+		},
+		Resources: corev1.ResourceRequirements{
+			Requests: corev1.ResourceList{
+				"cpu":    resource.MustParse("100m"),
+				"memory": resource.MustParse("64Mi"),
+			},
+			Limits: corev1.ResourceList{
+				"cpu":    resource.MustParse("100m"),
+				"memory": resource.MustParse("64Mi"),
+			},
+		},
+		VolumeMounts: []corev1.VolumeMount{
+			{
+				Name:      "oauth-config",
+				MountPath: "/etc/oauth/config",
+			},
+			{
+				Name:      "tls-certificates",
+				MountPath: "/etc/tls/private",
+			},
+		},
+	}
+
+	// Add the sidecar container to the notebook
+	notebookContainers := &notebook.Spec.Template.Spec.Containers
+	proxyContainerExists := false
+	for index, container := range *notebookContainers {
+		if container.Name == "oauth-proxy" {
+			(*notebookContainers)[index] = proxyContainer
+			proxyContainerExists = true
+			break
+		}
+	}
+	if !proxyContainerExists {
+		*notebookContainers = append(*notebookContainers, proxyContainer)
+	}
+
+	// Add the OAuth configuration volume:
+	// https://pkg.go.dev/k8s.io/api/core/v1#Volume
+	notebookVolumes := &notebook.Spec.Template.Spec.Volumes
+	oauthVolumeExists := false
+	oauthVolume := corev1.Volume{
+		Name: "oauth-config",
+		VolumeSource: corev1.VolumeSource{
+			Secret: &corev1.SecretVolumeSource{
+				SecretName:  notebook.Name + "-oauth-config",
+				DefaultMode: pointer.Int32Ptr(420),
+			},
+		},
+	}
+	for index, volume := range *notebookVolumes {
+		if volume.Name == "oauth-config" {
+			(*notebookVolumes)[index] = oauthVolume
+			oauthVolumeExists = true
+			break
+		}
+	}
+	if !oauthVolumeExists {
+		*notebookVolumes = append(*notebookVolumes, oauthVolume)
+	}
+
+	// Add the TLS certificates volume:
+	// https://pkg.go.dev/k8s.io/api/core/v1#Volume
+	tlsVolumeExists := false
+	tlsVolume := corev1.Volume{
+		Name: "tls-certificates",
+		VolumeSource: corev1.VolumeSource{
+			Secret: &corev1.SecretVolumeSource{
+				SecretName:  notebook.Name + "-tls",
+				DefaultMode: pointer.Int32Ptr(420),
+			},
+		},
+	}
+	for index, volume := range *notebookVolumes {
+		if volume.Name == "tls-certificates" {
+			(*notebookVolumes)[index] = tlsVolume
+			tlsVolumeExists = true
+			break
+		}
+	}
+	if !tlsVolumeExists {
+		*notebookVolumes = append(*notebookVolumes, tlsVolume)
+	}
+
+	// Set a dedicated service account, do not use default
+	notebook.Spec.Template.Spec.ServiceAccountName = notebook.Name
+	return nil
+}
+
+// Handle transforms the Notebook objects.
+func (w *NotebookWebhook) Handle(ctx context.Context, req admission.Request) admission.Response {
+	notebook := &nbv1.Notebook{}
+
+	err := w.decoder.Decode(req, notebook)
+	if err != nil {
+		return admission.Errored(http.StatusBadRequest, err)
+	}
+
+	if OAuthInjectionIsEnabled(*notebook) {
+		err = InjectOAuthProxy(notebook, w.OAuthConfig)
+		if err != nil {
+			return admission.Errored(http.StatusInternalServerError, err)
+		}
+	} else {
+		return admission.Allowed("Not injecting OAuth proxy")
+	}
+
+	marshaledNotebook, err := json.Marshal(notebook)
+	if err != nil {
+		return admission.Errored(http.StatusInternalServerError, err)
+	}
+
+	return admission.PatchResponseFromRaw(req.Object.Raw, marshaledNotebook)
+}
+
+// InjectDecoder injects the decoder.
+func (w *NotebookWebhook) InjectDecoder(d *admission.Decoder) error {
+	w.decoder = d
+	return nil
+}
